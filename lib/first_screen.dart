3d0ce09e127221477729320d4fa19ced372ecc79
craziie0023 <craziie0023@gmail.com>
2026-06-27 14:03:31 +0700

    Step 2: Install loading app screen

---
+++ lib/first_screen.dart
@@ -0,0 +1,71 @@
+// Step 2 : Install Loading app screen
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  //อะไรที่อยากจะให้ทำงานตอนเริ่มต้น ให่ใส่อันนี้
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SecondScreen()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.cyanAccent],
+          begin: FractionalOffset(0, 0),
+          end: FractionalOffset(0.5, 0.6),
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(child: Image.asset("./android/assets/image/spin.png")),
+          const SizedBox(height: 20),
+          const SpinKitSpinningLines(color: Colors.pinkAccent),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second Screen")),
+      body: Center(
+        child: Text(
+          "This is a second screen.",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.amberAccent,
+            fontWeight: FontWeight.w500,
+            fontFamily: "Alike",
+          ),
+        ),
+      ),
+    );
+  }
+}
