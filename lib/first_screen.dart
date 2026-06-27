efa0127fe63d52ce41e519ccaa777cddbc7b5356
craziie0023 <craziie0023@gmail.com>
2026-06-27 15:20:04 +0700

    Step 3: Check internet connection, Step 4:show toast message

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -2,6 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'dart:async';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
@@ -15,13 +18,49 @@ class _FirstScreenState extends State<FirstScreen> {
   //อะไรที่อยากจะให้ทำงานตอนเริ่มต้น ให่ใส่อันนี้
   void initState() {
     super.initState();
-    Timer(
-      const Duration(seconds: 3),
-      () => Navigator.push(
+
+    checkInternetConnection();
+  }
+
+  void checkInternetConnection() async {
+    final List<ConnectivityResult> connectivityResult = await (Connectivity()
+        .checkConnectivity());
+
+    // This condition is for demo purposes only to explain every connection type.
+    // Use conditions which work for your requirements.
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      showToast(context, "Mobile network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      showToast(context, "Wi-fi is available.");
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+    } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      showToast(context, "Ethernet connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      showToast(context, "Vpn connection active.");
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+    } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      showToast(context, "Bluetooth connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.satellite)) {
+      // Carrier-provided satellite network available
+      showToast(context, "Satellite network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      showToast(
         context,
-        MaterialPageRoute(builder: (context) => const SecondScreen()),
-      ),
-    );
+        "Connected to a network which is not in the above mentioned networks.",
+      );
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      showToast(context, "No available network types.");
+    }
   }
 
   @override
@@ -68,4 +107,62 @@ class SecondScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+//Step 4: Showtoast message
+void _timer(BuildContext context) {
+  // เมื่อครบ 3 วิให้ไปหน้า SecondScreen
+  Timer(
+    Duration(seconds: 3),
+    () => Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SecondScreen()),
+    ),
+  );
+}
+
+//Step 4: Show toast message
+void showToast(BuildContext context, String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.lightGreenAccent,
+    textColor: Colors.black,
+    fontSize: 24,
+  );
+}
+
+void showdialog(BuildContext context, String title, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: Text(message),
+        actions: <Widget>[
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Close',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 112, 150),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }
